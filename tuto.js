390658fa425badef4c7f20c1ac3602d40c3b1297
LEVASSEUR Romain <95504997+LEVASSEUR-Romain@users.noreply.github.com>
2022-07-07 17:06:24 +0200

    Master (#1)
    * first
    
    * fichier tuto modifichier tuto modifichier tuto modifff

---
+++ tuto.js
@@ -0,0 +1,11 @@
+// package script
+// on peut ajouter des script qui nous permet de simplifier les commandes
+// dans le terminal  "start": "node app.js"
+//on peut faire npm run start ou npm start
+
+// commande terminal : npm i express --save (--save ajoute dans package.json)
+
+// git hub
+// git status // regarder le statut actuel
+// git remote add origin url  // pour recuperer le git init de l'url
+// git fetch // tcheck sur origin le nouvelle ref de fichier (les ref pas le code)
