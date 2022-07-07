0eae043f04ae3383a3592f0b9e355ca02c167e75
Romain <romainlev@gmail.com>
2022-07-07 16:13:25 +0200

    first

---
+++ tuto.js
@@ -0,0 +1,6 @@
+// package script
+// on peut ajouter des script qui nous permet de simplifier les commandes
+// dans le terminal  "start": "node app.js"
+//on peut faire npm run start ou npm start
+
+// commande terminal : npm i express --save (--save ajoute dans package.json)
